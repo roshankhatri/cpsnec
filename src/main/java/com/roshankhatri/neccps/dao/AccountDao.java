33de267cb11e21ea50b7e121287ad663e20571cc
Roshan Khatri <roshank@nec.edu.np>
2016-03-30 08:50:23 +0545

    Modification Status upgraded

--- src/main/java/com/roshankhatri/neccps/dao/AccountDao.java
+++
@@ -1,9 +0,0 @@
-package com.roshankhatri.neccps.dao;
-
-import com.roshankhatri.neccps.model.Account;
-
-public interface AccountDao {
-	long save(Account account);
-	long update(Account account);
-
-}
